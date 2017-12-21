20e09abe076b73096d793827fdfb1c29142e82ef
yuebanyishenqiu <thisispwj@outlook.com>
2017-12-21 23:30:07 +0800

    bugs fixed

--- src/searchInsert.cc
+++ src/searchInsert.cc
@@ -11,12 +11,26 @@ int searchInsert(const vector<int>& vec,const int val){
         if(val == vec[k])
             return k;
         else if(val > vec[k]){
+            if( k + 1 < vec.size()){
+                if(vec[k+1] >= val)
+                    return k+1;
+            }else{
+                return vec.size();
+            }
             i = k + 1;
         }else{
+            if( k - 1 >= 0){
+                if(vec[k-1] < val)
+                    return k;
+                else if(vec[k-1] == val)
+                    return k-1;
+            }else{
+                return 0;
+            }
             j = k;
         }
     }
-    return 0;
+    //return 0;
 }
 
 int main(int argc, char* argv[]){
